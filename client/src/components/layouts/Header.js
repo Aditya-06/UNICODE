71e97ff038c692e44ec2797a3d844caef8f8144b
Aditya <adityaajmera060401@gmail.com>
2020-11-10 02:36:33 +0530

    Navbar

--- client/src/components/layouts/Header.js
+++ client/src/components/layouts/Header.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import AuthOptions from '../../../../client2/uber/src/components/auth/AuthOptions';
+import AuthOptions from '../auth/AuthOptions';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 export default function Header() {
+  const classes = useStyles();
   return (
     <div>
-      <Link to='/'>
-        <h1>Uber Clone</h1>
-      </Link>
-      <AuthOptions />
+      <AppBar
+        position='static'
+        style={{ backgroundColor: 'rgba(39, 39, 39)', color: 'white' }}
+      >
+        <Toolbar>
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+            aria-label='menu'
+          ></IconButton>
+
+          <Typography variant='h6' className={classes.title}>
+            <Link to='/' style={{ color: 'white', textDecoration: 'none' }}>
+              <i className='fas fa-taxi' style={{ fontSize: '20px' }} /> Uber
+              Clone
+            </Link>
+          </Typography>
+          <AuthOptions />
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
