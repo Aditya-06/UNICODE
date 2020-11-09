52731ea4954cc4b01a48e11e3cd2f5baaaae67dd
Aditya <adityaajmera060401@gmail.com>
2020-11-10 02:38:01 +0530

    Static info for Landing Page

---
+++ client/src/static/customer.js
@@ -0,0 +1,8 @@
+const customer = {
+  title: 'Customer',
+  desc:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+  imageURL: process.env.PUBLIC_URL + '/assets/card2.jpg',
+};
+
+export default customer;
