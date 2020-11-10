8d0389e7a9087c70de6e87741647c0ca471ac526
Aditya <adityaajmera060401@gmail.com>
2020-11-10 08:04:39 +0530

    Auth Routes Test

--- server/test/auth.test.js
+++ server/test/auth.test.js
@@ -1,18 +1,79 @@
+/* eslint-disable no-unused-vars */
+// Require the dev-dependencies
 const chai = require('chai');
+const assert = require('assert');
 
+const chaiHttp = require('chai-http');
+
+const mongoose = require('mongoose');
+const User = require('../models/user');
+const server = require('../app');
+const db = require('../config/db');
+
+const should = chai.should();
 const { expect } = chai;
-const validator = require('../isNumValid');
+chai.use(chaiHttp);
 
-describe('validator isNumValid()', () => {
-	it('should return true for a number in between 10 and 70', () => {
-		expect(validator.isNumValid(39)).to.be.true;
-	});
+const userCredentials = {
+	name: 'Aditya',
+	username: 'aditya@gmail.com',
+	password: 'qwerty',
+	conatct: '911',
+	role: 'user',
+};
 
-	it('should return false when the number is less than or equal to 10', () => {
-		expect(validator.isNumValid(10)).to.be.false;
+const driverCredentials = {
+	name: 'Ajmera',
+	username: 'ajmera@gmail.com',
+	password: 'qwerty',
+	conatct: '999',
+	role: 'driver',
+};
+// Our parent block
+describe('Auth Routes API Testing', () => {
+	before((done) => {
+		db.db().then(() => done());
 	});
+	after(async () => {
+		await User.remove({});
+		await db.close();
+	});
+	it('Register a user', (done) => {
+		const user = {
+			name: 'Aditya',
+			username: 'aditya@gmail.com',
+			password: 'qwerty',
+			conatct: '911',
+			role: 'user',
+			password2: 'qwerty',
+		};
+		chai
+			.request(server)
+			.post('/register')
+			.send(user)
+			.then((res) => {
+				expect(res.body.succes).to.equal(true);
+			})
+			.catch(done);
+	});
+	/*
+	it('Loggin in a user', (done) => {
+		const user = {
+			username: 'aditya@gmail.com',
+			password: 'qwerty',
+		};
+		chai
+			.request(server)
+			.post('/register')
+			.send(user)
+			.end((err, res) => {
+				res.should.be.a('object');
+				expect(res.body.success).to.equal(true);
+				done();
+			})
+			.catch(done);
+	});
+	*/
 
-	it('should return false when the number is greater than or equal to 70', () => {
-		expect(validator.isNumValid(79)).to.be.false;
-	});
+	// testing register
 });
